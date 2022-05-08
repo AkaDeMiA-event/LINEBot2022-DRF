05f201ec577aa9c0c71a317f5948967dee159f96
nikedsb <ikedanao1230sbsnkr@gmail.com>
2022-05-09 00:40:00 +0900

    for.pyの例追加

--- for.py
+++ for.py
@@ -2,3 +2,10 @@ def main():
     array = ["あ", "い", "う"]
     for data in array:
         print(data)
+
+    array2 = ["い", "ろ", "は", "に", "ほ", "へ", "と"]
+    for i, character in enumerate(array2):
+        print(i, character)
+
+
+main()
